8287171711127138217718b29c707d8495fd9e1f
AbnerLin <shaoyang0805@gmail.com>
2015-06-01 19:35:41 +0800

    update ing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var dateFormat = require('dateformat');
+var db = require('../mysql.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var connection = db();
+  connection.query('SELECT content, FROM_UNIXTIME(UNIX_TIMESTAMP(time)) as time FROM MESSAGE', function(error, results) {
+    if(error)
+      throw error;
+    console.log(results);    
+    results.forEach(function(item) {
+      item.time = dateFormat(item.time, "yyyy-mm-dd HH:MM");
+    });
+    res.render('index', {title: 'Yuntech - MIS', message: results});
+  });
 });
 
 module.exports = router;
